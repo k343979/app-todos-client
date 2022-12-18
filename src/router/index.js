30874683ea11088ddf36df5d0e37a01ef33c02d1
k343979 <yusuke040989@gmail.com>
2022-12-18 14:31:13 +0900

    不要ファイル削除

--- src/router/index.js
+++ src/router/index.js
@@ -21,14 +21,7 @@ import Application from "../views/settings/Application.vue";
 import Reminder from "../views/settings/Reminder.vue";
 import External from "../views/settings/External.vue";
 
-import TaskCreate from "../views/TaskCreate.vue"
-// import TaskDone from "../views/TaskDone.vue"
-// import TaskArchive from "../views/TaskArchive.vue"
-import Notification from "../views/Notification.vue"
-import UserAccount from "../views/UserAccount.vue"
-// import Application from "../views/Application.vue"
-// import Reminder from "../views/Reminder.vue"
-// import External from "../views/External.vue"
+// import TaskCreate from "../views/TaskCreate.vue"
 
 export default new VueRouter({
     mode: "history",
@@ -129,45 +122,10 @@ export default new VueRouter({
                 },
             ]
         },
-        {
-            path: "/task/create",
-            name: "task-create",
-            component: TaskCreate,
-        },
         // {
-        //     path: "/task/done",
-        //     name: "task-done",
-        //     component: TaskDone,
+        //     path: "/task/create",
+        //     name: "task-create",
+        //     component: TaskCreate,
         // },
-        // {
-        //     path: "/task/archive",
-        //     name: "task-archive",
-        //     component: TaskArchive,
-        // },
-        {
-            path: "/notification",
-            name: "notification",
-            component: Notification,
-        },
-        {
-            path: "/setting/user-account",
-            name: "user-account",
-            component: UserAccount,
-        },
-        {
-            path: "/setting/application",
-            name: "application",
-            component: Application,
-        },
-        {
-            path: "/setting/reminder",
-            name: "reminder",
-            component: Reminder,
-        },
-        {
-            path: "/setting/external",
-            name: "external",
-            component: External,
-        },
     ]
 });
